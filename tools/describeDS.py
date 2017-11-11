726a1aadcbda287216d97299b0309498f85e0d3f
duinodu <472365351duino@gmail.com>
2017-11-11 09:10:48 +0800

    add descsribeDS.py

--- tools/describeDS.py
+++ tools/describeDS.py
@@ -1,33 +1,178 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-'''
-python describeDataset.py [imdb name]
-eg: voc_2007_trainval, dian_2016_trainval, nest_2017_trainval
-'''
-
+from __future__ import print_function
+import argparse
 import sys, os
-this_dir = os.path.join(os.getcwd(), os.path.dirname(__file__))
-root = os.path.join(this_dir, '../')
-sys.path.insert(0, root)
-
-from objdect.dataset import datasets
-from objdect.train.prepare_roidb import get_training_roidb
 import numpy as np
 import matplotlib.pyplot as plt
+import torch.utils.data as data
+import xml.etree.ElementTree as ET
 
-def _convert2classes_roidb(roidb, imdb):
+
+class AnnotationTransform(object):
+    """Transforms a VOC annotation into a Tensor of bbox coords and label index
+    Initilized with a dictionary lookup of classnames to indexes
+
+    Arguments:
+        class_to_ind (dict, optional): dictionary lookup of classnames -> indexes
+            (default: alphabetic indexing of VOC's 20 classes)
+        keep_difficult (bool, optional): keep difficult instances or not
+            (default: False)
+        height (int): height
+        width (int): width
+    """
+    def __init__(self, class_to_ind=None, keep_difficult=False):
+        self.keep_difficult = keep_difficult
+
+    def __call__(self, target, width, height):
+        """
+        Arguments:
+            target (annotation) : the target annotation to be made usable
+                will be an ET.Element
+        Returns:
+            a list containing lists of bounding boxes  [bbox coords, class name]
+        """
+        res = []
+        for obj in target.iter('object'):
+            difficult = int(obj.find('difficult').text) == 1
+            if not self.keep_difficult and difficult:
+                continue
+            name = obj.find('name').text.lower().strip()
+            bbox = obj.find('bndbox')
+
+            pts = ['xmin', 'ymin', 'xmax', 'ymax']
+            bndbox = []
+            for i, pt in enumerate(pts):
+                cur_pt = int(bbox.find(pt).text) - 1
+
+                # scale height or width
+                #cur_pt = cur_pt / width if i % 2 == 0 else cur_pt / height
+                bndbox.append(cur_pt)
+
+            bndbox.append(name)
+            res += [bndbox]  # [xmin, ymin, xmax, ymax, name]
+
+            # img_id = target.find('filename').text[:-4]
+
+        return res  # [[xmin, ymin, xmax, ymax, name], ... ]
+
+class VOCDetection(data.Dataset):
+    """VOC Detection Dataset Object
+
+    input is image, target is annotation
+
+    Arguments:
+        root (string): filepath to VOCdevkit folder.
+        image_set (string): imageset to use (eg. 'train', 'val', 'test')
+        transform (callable, optional): transformation to perform on the
+            input image
+        target_transform (callable, optional): transformation to perform on the
+            target `annotation`
+            (eg: take in caption string, return tensor of word indices)
+        dataset_name (string, optional): which dataset to load
+            (default: 'VOC2007')
+    """
+
+    def __init__(self, root, image_sets, transform=None, target_transform=None,
+                 dataset_name='VOC2007'):
+        self.rootpath = root
+        self.image_sets = image_sets
+        self.transform = transform
+        self.target_transform = target_transform
+        self.name = dataset_name
+        self._annopath = os.path.join('%s', 'Annotations', '%s.xml')
+        self._imgpath = os.path.join('%s', 'JPEGImages', '%s.jpg')
+        self.ids = list()
+        for name in self.image_sets:
+            for line in open(os.path.join(self.rootpath, 'ImageSets', 'Main', name + '.txt')):
+                self.ids.append((self.rootpath, line.strip()))
+
+    def __getitem__(self, index):
+        im, gt, h, w = self.pull_item(index)
+        return im, gt
+
+    def __len__(self):
+        return len(self.ids)
+
+    def pull_item(self, index):
+        img_id = self.ids[index]
+
+        target = ET.parse(self._annopath % img_id).getroot()
+        img = cv2.imread(self._imgpath % img_id)
+        height, width, channels = img.shape
+        target = self.target_transform(target, width, height)
+
+        if self.transform is not None:
+            target = np.array(target)
+            try:
+                img, boxes, labels = self.transform(img, target[:, :4], target[:, 4])
+            except Exception as e:
+                import ipdb
+                ipdb.set_trace()
+            # to rgb
+            img = img[:, :, (2, 1, 0)]
+            # img = img.transpose(2, 0, 1)
+            target = np.hstack((boxes, np.expand_dims(labels, axis=1)))
+        return torch.from_numpy(img).permute(2, 0, 1), target, height, width
+        # return torch.from_numpy(img), target, height, width
+
+    def pull_image(self, index):
+        '''Returns the original image object at index in PIL form
+
+        Note: not using self.__getitem__(), as any transformations passed in
+        could mess up this functionality.
+
+        Argument:
+            index (int): index of img to show
+        Return:
+            PIL img
+        '''
+        img_id = self.ids[index]
+        return cv2.imread(self._imgpath % img_id, cv2.IMREAD_COLOR)
+
+    def pull_anno(self, index):
+        '''Returns the original annotation of image at index
+
+        Note: not using self.__getitem__(), as any transformations passed in
+        could mess up this functionality.
+
+        Argument:
+            index (int): index of img to get annotation of
+        Return:
+            list:  [img_id, [(label, bbox coords),...]]
+                eg: ('001718', [('dog', (96, 13, 438, 332))])
+        '''
+        img_id = self.ids[index]
+        anno = ET.parse(self._annopath % img_id).getroot()
+        gt = self.target_transform(anno, 1, 1)
+        return img_id[1], gt
+
+    def pull_tensor(self, index):
+        '''Returns the original image at an index in tensor form
+
+        Note: not using self.__getitem__(), as any transformations passed in
+        could mess up this functionality.
+
+        Argument:
+            index (int): index of img to show
+        Return:
+            tensorized version of img, squeezed
+        '''
+        return torch.Tensor(self.pull_image(index)).unsqueeze_(0)
+
+
+def _convert2classes(ds):
+    rois_dict = {}
+    for i in range(len(ds)):
+        _, gt = ds.pull_anno(i)
+        for roi in gt:
+            if roi[-1] not in rois_dict:
+                rois_dict[roi[-1]] = []
+            rois_dict[roi[-1]].append(roi[:4])
     results = {}
-    for (name, each_class_ind) in imdb._class_to_ind.items(): 
-        if each_class_ind == 0:
-            continue
-        bboxes = []
-        for roi in roidb:
-            for ind, label in enumerate(roi['gt_classes']):
-                if label== each_class_ind:
-                    bboxes.append(roi['boxes'][ind])
-        results[name] = np.array(bboxes)
-        print(name, results[name].shape)
+    for name in rois_dict.keys():
+        results[name] = np.array(rois_dict[name])
     return results
 
 
@@ -90,16 +235,12 @@ def drawBar(data, imdb_name):
     plt.show()
 
 
-def describeDataset(argv):
-    imdb_name = argv[1]
-    dataset_path = os.path.join(os.environ['HOME'], 'data/VOCdevkit')
-    imdb = datasets.get_imdb_common(imdb_name, dataset_path)
+def describeDataset(args):
+    ds_name = os.path.basename(args.dataset_path)
+    ds = VOCDetection(args.dataset_path, args.split, target_transform=AnnotationTransform())
 
-    n_classes = imdb.num_classes
-    roidb = get_training_roidb(imdb, False)
-    class_bboxes = _convert2classes_roidb(roidb, imdb)
-
-    
+    print('Reading anno files...')
+    class_bboxes = _convert2classes(ds)
 
     num_boxes = {}
     size_boxes = {}
@@ -115,16 +256,15 @@ def describeDataset(argv):
         ratio_boxes[k] = [np.mean(ratio), np.std(ratio)]
 
     #print sorted(num_boxes.items(), lambda x, y: cmp(x[1], y[1]), reverse=True)
-    drawBar([num_boxes, size_boxes, ratio_boxes], imdb_name)
+    drawBar([num_boxes, size_boxes, ratio_boxes], ds_name)
 
 
-def main():
-    import sys
-    if len(sys.argv) != 2:
-        print(__doc__)
-        return
-    describeDataset(sys.argv)
+def main(args):
+    describeDataset(args)
 
 if __name__ == "__main__":
-    raise NotImplementedError
-    main()
+    parser = argparse.ArgumentParser(description='Description of voc-like dataset.')
+    parser.add_argument('--dataset_path', required=True, type=str, help='dataset directory')
+    parser.add_argument('--split', nargs='+', required=True, help='train | trainval | test')
+    args = parser.parse_args()
+    main(args)
